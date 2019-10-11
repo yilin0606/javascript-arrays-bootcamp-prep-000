4fb6005a0182be0fb3bf873ea991d4531c753b14
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-11 22:08:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyAddElementToEndOfArray(array,element){
   }
   
  function accessElementInArray(array,index){
-    return console.log(array[index]);
+    return array[index];
   }
  
  
